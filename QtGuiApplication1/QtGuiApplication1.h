40232480c2cddc520620882c3c51d2f25b5dba91
stongelidis <s.tongelidis@gmail.com>
2019-06-12 12:25:48 -0400

    enables saving of plot to .png file

--- QtGuiApplication1/QtGuiApplication1.h
+++ QtGuiApplication1/QtGuiApplication1.h
@@ -71,6 +71,8 @@ public:
 	void reset_color_correction();
 	void allow_epoch();
 	void plot_full_data(int index);
+	void save_plot();
+	void save_frame();
 	
 private:
 	Ui::QtGuiApplication1Class ui;
