aea9c760d871fd99bdb08a943a51920ed16822d7
stongelidis <s.tongelidis@gmail.com>
2019-08-04 14:35:13 -0400

    list all json files within directory. on selection shows file contents

--- QtGuiApplication1/location_input.h
+++ QtGuiApplication1/location_input.h
@@ -2,25 +2,36 @@
 
 #include <ui_location_gui.h>
 
+#include "logging.h"
+
 #include <qlabel.h>
 #include <qpushbutton.h>
 #include <qobject.h>
 #include <qdialog.h>
 #include <qdir.h>
+#include <qjsondocument.h>
+#include <qjsonobject.h>
+#include <qjsonvalue.h>
 
 
-class LocationInput : public QDialog
+class LocationInput : public QDialog//, public QWidget
 {
 	Q_OBJECT
 
 public:
 
 	QString selected_file_path;
+	QString directory_path;
 
 	LocationInput();
 	~LocationInput();
 
+	void RefreshListBox();
+
+public slots:
+	void OnItemChange(QString item);
+
 private:
 	Ui_Dialog ui;
-	int num;
+
 };
