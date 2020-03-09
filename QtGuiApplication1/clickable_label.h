a146c1a1b74b862ef8e66912013d5c2061918b7a
stongelidis <s.tongelidis@gmail.com>
2020-03-09 17:20:44 -0400

    code clean-up

--- QtGuiApplication1/clickable_label.h
+++ QtGuiApplication1/clickable_label.h
@@ -36,21 +36,5 @@ protected:
 
 };
 
-/*
-class ClickableLabel : public QLabel {
-	Q_OBJECT
-
-public:
-	explicit ClickableLabel(QWidget* parent = Q_NULLPTR, Qt::WindowFlags f = Qt::WindowFlags());
-	~ClickableLabel();
-
-signals:
-	void clicked();
-
-protected:
-	void mousePressEvent(QMouseEvent* event);
-}
-*/
-
 
 #endif // CLICKABLE_LABEL_H
