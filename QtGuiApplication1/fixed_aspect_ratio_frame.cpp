71af0ece9772203d545cd633a4ec5cad3ddcbb48
stephanos.tongelidis <stephanos.tongelidis@fts-intl.com>
2021-03-23 09:55:08 -0400

    adds class for fixed aspec ratio frame

---
+++ QtGuiApplication1/fixed_aspect_ratio_frame.cpp
@@ -0,0 +1,43 @@
+#include "fixed_aspect_ratio_frame.h"
+
+FixedAspectRatioFrame::FixedAspectRatioFrame(QWidget* parent) :QFrame(parent)
+{
+
+	aspect_ratio_width = 4;
+	aspect_ratio_height = 3;
+	fixed_aspect_ratio = false;
+
+}
+
+FixedAspectRatioFrame::~FixedAspectRatioFrame()
+{
+
+}
+
+void FixedAspectRatioFrame::set_aspect_ratio(int width, int height)
+{
+	aspect_ratio_width = width;
+	aspect_ratio_height = height;
+}
+
+void FixedAspectRatioFrame::enable_fixed_aspect_ratio(bool enable)
+{
+
+	fixed_aspect_ratio = enable;
+
+}
+
+void FixedAspectRatioFrame::resizeEvent(QResizeEvent* event)
+{
+
+	if (fixed_aspect_ratio) {
+
+
+		double ar = aspect_ratio_width * 1.0 / aspect_ratio_height;
+		int new_width = ar * this->height();
+
+		this->setMinimumWidth(new_width);
+
+	}
+
+}
