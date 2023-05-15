705c0abfe6ebca02cd0ebce3c6518b6c79f9fd04
Kuhn <jordan.kuhn@maxar.com>
2023-05-15 15:13:55 -0400

    Control Processing State Changes Through QComboBox
    Please note the UI at this state is a bit broken - some of the "old"
    control features like the checkboxes still exist and control state
    changes - and a lot of code and data is stored redundantly in the "old"
    methods. Ideally, a clean delete of the old control features (e.g. the
    entire `something` vector) should now be feasible and the entire control
    can flow through the new features.
    
    The new capability chains events together in a vector. The chain of past
    processing operations allow users to switch between any desired states
    at their leisure.
    
    This chain can now trivially be converted into workspace saves/loads!
    
    This commit is equivalent to the end of PR #12.

--- SirveApp/video_container.cpp
+++ SirveApp/video_container.cpp
@@ -2,7 +2,7 @@
 
 Video_Container::Video_Container()
 {
-
+	current_idx = -1;
 }
 
 Video_Container::~Video_Container()
@@ -11,16 +11,48 @@ Video_Container::~Video_Container()
 	processing_states.clear();
 }
 
+void Video_Container::select_state(int idx)
+{
+	current_idx = idx;
+
+	emit update_display_video(processing_states[idx]);
+}
+
 void Video_Container::clear_processing_states()
 {
 	processing_states.clear();
-	emit processing_states_cleared();
+	current_idx = -1;
+
+	emit states_cleared();
 }
 
 void Video_Container::add_processing_state(video_details new_details)
 {
 	processing_states.push_back(new_details);
-	emit processing_state_added(QString::number(processing_states.size()));
+	current_idx = processing_states.size() - 1;
+
+	emit state_added(QString::number(current_idx), current_idx);
+	emit update_display_video(new_details);
+}
+
+video_details Video_Container::copy_current_state()
+{
+	return processing_states[current_idx];
+}
+
+void Video_Container::undo()
+{
+	if (processing_states.size() < 2) {
+		return;
+	}
+
+	processing_states.pop_back();
+	emit state_removed(processing_states.size());
+
+	if (current_idx == processing_states.size()) {
+		current_idx -= 1;
+		emit update_display_video(processing_states[current_idx]);
+	}
 }
 
 void Video_Container::reset(video_details input)
