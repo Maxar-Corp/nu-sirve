5013bdb826feb654ee9246d583dc16f616174cc5
Kuhn <jordan.kuhn@maxar.com>
2023-05-15 13:56:03 -0400

    Add Vector for Processing States
    This is not currently interactable but is a stub for future behavior.
    
    Moving away from the weird "property->bool" mapping that limits us to 1
    video per processing state is essential to allow viewing and interacting
    with the history of processing steps.
    
    The future (saving the workspace) will run through this dropdown and
    vector.

--- SirveApp/video_container.h
+++ SirveApp/video_container.h
@@ -4,6 +4,8 @@
 #include <vector>
 #include <qobject.h>
 
+#include <QString>
+
 #include "logging.h"
 #include "video_details.h"
 
@@ -13,9 +15,13 @@ class Video_Container : public QObject
 	
 	public:
 		std::vector<video_details> something;
+		std::vector<video_details> processing_states;
 
 		Video_Container();
 		~Video_Container();
+
+		void Video_Container::clear_processing_states();
+		void Video_Container::add_processing_state(video_details new_details);
 
 		void reset(video_details input);
 		void display_original_data();
@@ -25,6 +31,8 @@ class Video_Container : public QObject
 	signals:
 		void update_display_video(video_details &update);
 
+		void processing_states_cleared();
+		void processing_state_added(QString name);
 
 	private:
 
