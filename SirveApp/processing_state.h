85608e4a6e62753806251a5cc0e6bda995895967
Kuhn <jordan.kuhn@maxar.com>
2023-05-17 11:42:13 -0400

    Add Processing Step Method-Specific Values
    Includes adding the v1.3 "new" fields to the processing_state struct.

--- SirveApp/processing_state.h
+++ SirveApp/processing_state.h
@@ -16,8 +16,10 @@ struct processing_state {
     //NOTE: This is a poor implementation of "polymorphic" configuration but can be cleaned up in a future refactor
     //These fields will only contain a value if the Processing_Method field is set to indicate they should
     //The burden is on consumers of the processing_state struct to correctly interpret the fields
-    int background_sub_num_frames;
+    int bgs_relative_start_frame;
+    int bgs_num_frames;
 
+    QString nuc_file_path;
     int nuc_start_frame;
     int nuc_stop_frame;
 
