4af82a21235dcbd60c073ac034a5f10ac96b4667
Kuhn <jordan.kuhn@maxar.com>
2023-05-16 20:37:42 -0400

    Remove Unused Code, Remove 'something' Vector
    Now that the video_container has been reworked, a lot of the previous
    code is unused.
    
    After this was initially committed in PR #13, new references to the
    `something` vector were added in version 1.3. The previous commit
    removed some of those references, but this commit had to add some
    additional changes to accomodate that removal. As a byproduct of the
    `video_container` now being owned by the video_display class, I had to
    cludge together a fix by allowing direct access to the current
    processing state.
    
    I don't love the fact that the video_container "leaks" its behavior like
    this now, but don't want to mess too much with history yet to fix it. I
    will leave this to clean up later.

--- SirveApp/video_container.cpp
+++ SirveApp/video_container.cpp
@@ -7,7 +7,6 @@ Video_Container::Video_Container()
 
 Video_Container::~Video_Container()
 {
-	something.clear();
 	processing_states.clear();
 }
 
@@ -58,45 +57,4 @@ void Video_Container::undo()
 		current_idx -= 1;
 		emit update_display_video(processing_states[current_idx]);
 	}
-}
-
-void Video_Container::reset(video_details input)
-{
-	something.clear();
-	something.push_back(input);
-	emit update_display_video(input);
-}
-
-void Video_Container::display_original_data()
-{
-	emit update_display_video(something[0]);
-}
-
-bool Video_Container::display_data(video_details input)
-{	
-	for (int i = 0; i < something.size(); i++)
-	{
-		if (input == something[i]) {
-			emit update_display_video(something[i]);
-			DEBUG << "Video Container: Updating display video to index of " << i;
-			return true;
-		}
-	}
-
-	return false;
-}
-
-int Video_Container::find_data_index(video_details input)
-{
-	int number_videos = something.size();
-
-	for (int i = 0; i < number_videos; i++)
-	{
-		if (input == something[i]) {
-			DEBUG << "Video Container: Match for video found at index " << i;
-			return i;
-		}
-	}
-
-	return -1;
 }
--- SirveApp/video_details.cpp
+++ SirveApp/video_details.cpp
@@ -1,16 +1,7 @@
 #include "video_details.h"
 
 video_details::video_details() {
-
 	number_of_bits = 0;
-
-	int number_of_enumerations = (int)Video_Parameters::number_of_parameters;
-
-	for (int i = 0; i < number_of_enumerations; i++)
-	{
-		properties.insert(std::pair<Video_Parameters, bool>(Video_Parameters(i), false));
-	}
-
 }
 
 video_details::~video_details()
@@ -39,20 +30,4 @@ void video_details::set_video_frames(std::vector<std::vector<uint16_t>> video_in
 void video_details::clear_16bit_vector()
 {
 	frames_16bit.clear();
-}
-
-template <typename Map>
-bool video_details::map_compare(Map const &lhs, Map const &rhs) {
-	// No predicate needed because there is operator== for pairs already.
-	return lhs.size() == rhs.size()
-		&& std::equal(lhs.begin(), lhs.end(),
-			rhs.begin());
-}
-
-bool video_details::operator==(const video_details  &f) {
-
-	//https://stackoverflow.com/questions/16093413/comparison-operator-overloading-for-a-struct-symmetrically-comparing-my-struct
-	//https://stackoverflow.com/questions/8473009/how-to-efficiently-compare-two-maps-of-strings-in-c	
-
-	return map_compare(properties, f.properties);
 }
