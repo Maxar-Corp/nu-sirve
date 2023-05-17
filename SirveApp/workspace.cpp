83c9035ba24b54f6009b4918e43d88b6ca03b308
Kuhn <jordan.kuhn@maxar.com>
2023-05-17 16:15:43 -0400

    Persist Processing State Info to Workspace

--- SirveApp/workspace.cpp
+++ SirveApp/workspace.cpp
@@ -8,18 +8,56 @@ Workspace::~Workspace()
 {
 };
 
-void Workspace::save_state(QString image_path, int start_frame, int end_frame) {
+void Workspace::save_state(QString image_path, int start_frame, int end_frame, std::vector<processing_state> all_states) {
     //Inspiration: https://forum.qt.io/topic/65874/create-json-using-qjsondocument
     QJsonObject json_object;
     json_object.insert("image_path", image_path);
     json_object.insert("start_frame", start_frame);
     json_object.insert("end_frame", end_frame);
+
+    QJsonArray states;
+    for (auto state : all_states)
+    {
+        QJsonObject state_object = state_to_json(state);
+        states.push_back(state_object);
+    }
+    json_object.insert("processing_states", states);
+
     QJsonDocument json_document(json_object);
 
     QFile file(QString(WORKSPACE_FOLDER) + "/" + QString(WORKSPACE_FILE));
     file.open(QIODevice::WriteOnly|QIODevice::Text);
     file.write(json_document.toJson());
     file.close();
+};
+
+QJsonObject Workspace::state_to_json(const processing_state & state)
+{
+    QJsonObject state_object;
+    switch (state.method)
+    {
+        case Processing_Method::original:
+            state_object.insert("method", "Original");
+            break;
+        case Processing_Method::background_subtraction:
+            state_object.insert("method", "Background Subtraction");
+            state_object.insert("bgs_relative_start_frame", state.bgs_relative_start_frame);
+            state_object.insert("bgs_num_frames", state.bgs_num_frames);
+            break;
+        case Processing_Method::deinterlace:
+            state_object.insert("method", "Deinterlace");
+            state_object.insert("deint_type", QString::number(state.deint_type));
+            break;
+        case Processing_Method::non_uniformity_correction:
+            state_object.insert("method", "NUC");
+            state_object.insert("nuc_start_frame", state.nuc_start_frame);
+            state_object.insert("nuc_stop_frame", state.nuc_stop_frame);
+            state_object.insert("nuc_file_path", state.nuc_file_path);
+            break;
+        default:
+            state_object.insert("method", "error");
+    }
+    return state_object;
 };
 
 WorkspaceValues Workspace::load_state() {
