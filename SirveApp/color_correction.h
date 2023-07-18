78107607b748d411a580594b896447c29fcf92e6
Kuhn <jordan.kuhn@maxar.com>
2023-07-18 17:34:09 -0400

    Completely Remove Lift_Gamma_Gain Class
    Completely unused pile of code.

--- SirveApp/color_correction.h
+++ SirveApp/color_correction.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#ifndef COLOR_CORRECTION_LGG_H
-#define COLOR_CORRECTION_LGG_H
+#ifndef COLOR_CORRECTION_H
+#define COLOR_CORRECTION_H
 
 #include <iostream>
 #include <string>
@@ -9,35 +9,6 @@
 #include <armadillo>
 
 #include <QWidget>
-
-class Lift_Gamma_Gain : public QWidget
-{
-	Q_OBJECT
-public:
-
-	Lift_Gamma_Gain(double input_lift = 0, double input_gamma = 1, double input_gain = 1);
-	~Lift_Gamma_Gain();
-
-	double get_min_lift();
-	double get_lift();
-	double get_gamma();
-	double get_gain();
-	double get_updated_color(int original_value, int max_value);
-	void get_updated_color(arma::vec& input, int max_value, double &frame_min, double &frame_max);
-
-	bool set_lift(double value);
-	bool set_gamma(double value);
-	bool set_gain(double value);
-
-signals:
-	void update_lift_gamma_gain(double lift, double gamma, double gain);
-
-private:
-	double lift, gamma, gain;
-	double max_lift, min_lift, max_gamma, min_gamma, max_gain, min_gain;
-
-};
-
 
 class Min_Max_Value : public QWidget
 {
@@ -61,4 +32,4 @@ private:
 
 
 
-#endif // COLOR_CORRECTION_LGG_H
+#endif // COLOR_CORRECTION_H
