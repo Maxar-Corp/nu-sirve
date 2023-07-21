f2f6f6db76c5157e8eb1d67d86cc65fe530ab098
Kuhn <jordan.kuhn@maxar.com>
2023-07-21 09:42:07 -0400

    Completely Rework Bad Pixel Logic
    I lost the plot a bit and couldn't split out meaningful commits, so this
    commit bundles the whole change into one.
    
    Essentially, this commit changes the bad pixel process to impact the
    first processing_state itself. Instead of being a list of pixels that is
    stored alongside the states, it is now an intrinsic property of the
    first state. This is because, ultimately, if the user chooses to
    replace bad pixels, we want to straight up _replace_ the bad pixels with
    a local (kernel) average. The only reason to persist the list of ones we
    fixed is to be able to tell the user later which pixels were changed.
    
    As the pixels themselves will be altered, any later filters (background
    subtraction, etc.) will be based on the new (fake) value for the
    replaced pixels. This will have implications on any
    mathematical/scientific processing of the data.
    
    Things done in this change include moving the bad pixel UI controls to
    the processing tab, having a "confirmation" dialog when bad pixels are
    replaced via the UI, renaming bad_pixel_identification to bad_pixels
    since it now also includes replacement, and the change from a bad pixel
    "mask" (a per-pixel 0 or 1 depending on whether it was good or bad,
    respectively) into a list of bad pixel indeces.

--- SirveApp/bad_pixels.cpp
+++ SirveApp/bad_pixels.cpp
@@ -1,12 +1,12 @@
-#include "bad_pixel_identification.h"
+#include "bad_pixels.h"
 
-std::vector<short> BadPixelIdentification::get_dead_pixel_mask(const std::vector<std::vector<uint16_t>>& input_pixels)
+std::vector<unsigned int> BadPixels::identify_dead_pixels(const std::vector<std::vector<uint16_t>>& input_pixels)
 {    
-    std::vector<short> dead_pixel_mask;
+    std::vector<unsigned int> dead_pixels;
 
     size_t num_frames = input_pixels.size();
     // Loop through each pixel
-    for (int i = 0; i < input_pixels[0].size(); i++) 
+    for (auto i = 0; i < input_pixels[0].size(); i++)
     {
         double mean = 0.0;
         double squared_diff_sum = 0.0;
@@ -24,8 +24,16 @@ std::vector<short> BadPixelIdentification::get_dead_pixel_mask(const std::vector
         double variance = squared_diff_sum / num_frames;
         double std_deviation = std::sqrt(variance);
 
-        dead_pixel_mask.push_back(std_deviation == 0);
+        if (std_deviation == 0)
+        {
+            dead_pixels.push_back(i);
+        }
     }
 
-    return dead_pixel_mask;
+    return dead_pixels;
+}
+
+void BadPixels::replace_pixels_with_neighbors(std::vector<std::vector<uint16_t>> & original_pixels, std::vector<unsigned int> bad_pixel_indeces)
+{
+    //intentionally do nothing rn
 }
