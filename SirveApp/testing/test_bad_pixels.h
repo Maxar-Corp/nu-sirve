8ff92db2babf4c5b9c755f59121c381fbbcc4263
Kuhn <jordan.kuhn@maxar.com>
2023-07-24 15:42:40 -0400

    Implement Bad Pixel Replacement
    Uses a crosshairs pattern (up to 2 pixels above, below, left, and right
    of the bad pixel) to replace the value of a bad pixel with the local
    mean.
    
    Includes tests written to prove that it doesn't fail on edges and
    corners and correctly ignores other nearby bad pixels.

--- SirveApp/testing/test_bad_pixels.h
+++ SirveApp/testing/test_bad_pixels.h
@@ -8,6 +8,9 @@ public:
     TestBadPixels();
 private slots:
     void test_identify_dead_pixels();
+    void test_replace_bad_pixels_center();
+    void test_replace_bad_pixels_edges();
+    void test_replace_bad_pixels_overlapping();
 private:
     std::vector<std::vector<uint16_t>> generate_test_input_with_dead_pixels();
 };
