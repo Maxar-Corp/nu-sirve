0c769aed82a1f27e76f368248ebaa6f36c4fdedf
Kuhn <jordan.kuhn@maxar.com>
2023-07-28 10:36:49 -0400

    Make PopoutDialog take QWidget
    All the various things we popped out were QWidget-derived, so there's no
    need for the verbosity.

--- SirveApp/popout_dialog.cpp
+++ SirveApp/popout_dialog.cpp
@@ -1,27 +1,9 @@
 #include "popout_dialog.h"
 
-PopoutDialog::PopoutDialog(EnhancedLabel* label)
-{
-	mainLayout = new QGridLayout();
-    mainLayout->addWidget(label);
-	setLayout(mainLayout);
-
-    setWindowTitle(tr("Popout Display"));
-}
-
-PopoutDialog::PopoutDialog(Clickable_QChartView* chart_view)
+PopoutDialog::PopoutDialog(QWidget* widget)
 {
     mainLayout = new QGridLayout();
-    mainLayout->addWidget(chart_view);
-    setLayout(mainLayout);
-
-    setWindowTitle(tr("Popout Display"));
-}
-
-PopoutDialog::PopoutDialog(NewChartView* new_chart_view)
-{
-    mainLayout = new QGridLayout();
-    mainLayout->addWidget(new_chart_view);
+    mainLayout->addWidget(widget);
     setLayout(mainLayout);
 
     setWindowTitle(tr("Popout Display"));
