979ac7cb6c70b9c97dfb7f69f52f7a20a8cfcb02
Kuhn <jordan.kuhn@maxar.com>
2023-10-25 10:11:25 -0400

    Add forgotten unit test for zooming

--- SirveApp/testing/test_video_display_zoom.h
+++ SirveApp/testing/test_video_display_zoom.h
@@ -11,4 +11,5 @@ private slots:
     void test_zoom_and_unzoom();
     void test_get_position_within_zoom();
     void test_is_any_piece_within_zoom();
+    void test_zoom_resets_up_and_left_if_over_the_edge();
 };
