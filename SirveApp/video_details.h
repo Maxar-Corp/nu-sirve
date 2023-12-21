5db2544161e5f2813da0b4cd0451eb66084c88f8
Kuhn <jordan.kuhn@maxar.com>
2023-12-21 08:54:40 -0500

    Remove complexity from video_details struct

--- SirveApp/video_details.h
+++ SirveApp/video_details.h
@@ -3,18 +3,6 @@
 #include <vector>
 
 struct video_details {
-	int x_pixels, y_pixels, number_pixels;
-
+	int x_pixels, y_pixels;
 	std::vector<std::vector<uint16_t>> frames_16bit;
-	std::vector<std::vector<unsigned int>> histogram_data;
-
-	std::vector<std::vector<double>> boresight_pointing;
-	std::vector<std::vector<double>> tgt_az_el;
-
-	video_details();
-	~video_details();
-
-	void set_image_size(int x, int y);
-
-	void set_video_frames(std::vector<std::vector<uint16_t>> video_in);
 };
