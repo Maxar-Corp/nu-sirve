468c54f0c7569db244f993cb91ba76eaf9025d36
Kuhn <jordan.kuhn@maxar.com>
2024-02-16 15:42:28 -0500

    Remove 'track' from generic classes
    These were generic Qt classes that aren't really "track" specific
    (although they're only used by the track management widget right now) so
    removing the track idea makes them a bit cleaner.

--- SirveApp/support/qt_elements_with_ids.cpp
+++ SirveApp/support/qt_elements_with_ids.cpp
@@ -1,18 +1,18 @@
 #include "support/qt_elements_with_ids.h"
 
 QPushButtonWithId::QPushButtonWithId(int id, const QString& text, QWidget *parent)
-    : QPushButton(text, parent), track_id(id)
+    : QPushButton(text, parent), id(id)
 {
     connect(this, &QPushButtonWithId::clicked, this, &QPushButtonWithId::handle_button_click);
 }
 
 void QPushButtonWithId::handle_button_click()
 {
-    emit clicked_with_id(track_id);
+    emit clicked_with_id(id);
 }
 
 QCheckBoxWithId::QCheckBoxWithId(int id, const QString& text, QWidget *parent)
-    : QCheckBox(text, parent), track_id(id)
+    : QCheckBox(text, parent), id(id)
 {
     connect(this, &QCheckBox::stateChanged, this, &QCheckBoxWithId::handle_state_changed);
 }
@@ -21,10 +21,10 @@ void QCheckBoxWithId::handle_state_changed(int state)
 {
     if (state == Qt::Checked)
     {
-        emit checked_with_id(track_id);
+        emit checked_with_id(id);
     }
     else
     {
-        emit unchecked_with_id(track_id);
+        emit unchecked_with_id(id);
     }
 }
