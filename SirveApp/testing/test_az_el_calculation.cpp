ec4c75e3ff139f782b163df10af807ac4084b91f
Holland <david.holland@maxar.com>
2024-04-08 21:04:39 -0400

    Move the offset adjustments to the calculate function itself.

--- SirveApp/testing/test_az_el_calculation.cpp
+++ SirveApp/testing/test_az_el_calculation.cpp
@@ -17,9 +17,11 @@ void TestAzElCalculation::test_azimuth_elevation_calculation()
     std::vector<double> dcm_frame_0 = {1.10336495, -29.22956721, -32.26855777, 11.75914352, 31.27938875, -27.93147359, 41.92075815, -8.004815666, 8.684342038};
     std::vector<double> dcm_frame_1 = {1.101399250, -29.22489568, -32.26757275, 11.75588568, 31.27845844, -27.92778274, 41.91765655, -8.004209470, 8.680239582};
 
+	bool adjust_frame_ref = false;
+
     // Verify the boresight az/el from frame 0 matches reality
     int boresight_x = 0, boresight_y = 0;
-    std::vector<double> results = AzElCalculation::calculate(boresight_x, boresight_y, sensor_lat_frame_0, sensor_long_frame_0, dcm_frame_0, ifovx, ifovy);
+    std::vector<double> results = AzElCalculation::calculate(boresight_x, boresight_y, sensor_lat_frame_0, sensor_long_frame_0, dcm_frame_0, ifovx, ifovy, adjust_frame_ref);
     double data_export_expected_az = 99.904309, data_export_expected_el = 28.537447;
     QVERIFY(results[0] - delta <= data_export_expected_az && results[0] + delta >= data_export_expected_az);
     QVERIFY(results[1] - delta <= data_export_expected_el && results[1] + delta >= data_export_expected_el);
@@ -39,7 +41,7 @@ void TestAzElCalculation::test_azimuth_elevation_calculation()
     int pinpoint_track_x = 624, pinpoint_track_y = 206;
     int adjusted_track_x = pinpoint_track_x - 2 - 320; //THIS HAS TO SHIFT LEFT ONE EXTRA BECAUSE THE OSM CENTROID DOUBLE WAS ROUNDED TO AN INTEGER
     int adjusted_track_y = pinpoint_track_y - 1 - 240;
-    results = AzElCalculation::calculate(adjusted_track_x, adjusted_track_y, sensor_lat_frame_0, sensor_long_frame_0, dcm_frame_0, ifovx, ifovy);
+    results = AzElCalculation::calculate(adjusted_track_x, adjusted_track_y, sensor_lat_frame_0, sensor_long_frame_0, dcm_frame_0, ifovx, ifovy, adjust_frame_ref);
     data_export_expected_az = 100.245425, data_export_expected_el = 28.572941;
     QVERIFY(results[0] - delta <= data_export_expected_az && results[0] + delta >= data_export_expected_az);
     QVERIFY(results[1] - delta <= data_export_expected_el && results[1] + delta >= data_export_expected_el);
@@ -48,7 +50,7 @@ void TestAzElCalculation::test_azimuth_elevation_calculation()
     pinpoint_track_x = 624, pinpoint_track_y = 205;
     adjusted_track_x = pinpoint_track_x - 1 - 320;
     adjusted_track_y = pinpoint_track_y - 240; //THIS HAS TO SHIFT "UP" ONE (OFFSETTING) BECAUSE THE OSM CENTROID DOUBLE WAS ROUNDED TO AN INTEGER
-    results = AzElCalculation::calculate(adjusted_track_x, adjusted_track_y, sensor_lat_frame_1, sensor_long_frame_1, dcm_frame_1, ifovx, ifovy);
+    results = AzElCalculation::calculate(adjusted_track_x, adjusted_track_y, sensor_lat_frame_1, sensor_long_frame_1, dcm_frame_1, ifovx, ifovy, adjust_frame_ref);
     data_export_expected_az = 100.250388, data_export_expected_el = 28.569998;
     QVERIFY(results[0] - delta <= data_export_expected_az && results[0] + delta >= data_export_expected_az);
     QVERIFY(results[1] - delta <= data_export_expected_el && results[1] + delta >= data_export_expected_el);
