3a427849b1fefe1bf6bd7425d269dd9e8144b9c6
Holland <david.holland@maxar.com>
2024-04-08 21:04:39 -0400

    Move the offset adjustments to the calculate function itself.

--- SirveApp/support/az_el_calculation.h
+++ SirveApp/support/az_el_calculation.h
@@ -10,12 +10,11 @@
 const int FOCAL_PLANE_ARRAY_WIDTH = 640;
 const int FOCAL_PLANE_ARRAY_HEIGHT = 480;
 
-const int OSM_CENTROID_X_SHIFT = 2;
-const int OSM_CENTROID_Y_SHIFT = 1;
+const int OSM_CENTROID_SHIFT = 1;
 
 namespace AzElCalculation
 {
-    std::vector<double> calculate(int x, int y, double sensor_lat, double sensor_long, std::vector<double> dcm, double ifov_x, double ifov_y);
+    std::vector<double> calculate(int x, int y, double sensor_lat, double sensor_long, std::vector<double> dcm, double ifov_x, double ifov_y, bool adjust_frame_ref);
 };
 
 #endif
