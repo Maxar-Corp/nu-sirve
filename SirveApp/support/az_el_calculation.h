0d92f9d3dff3e559613c986323bef8395ebb24ad
Holland <david.holland@maxar.com>
2024-04-09 21:20:26 -0400

    Differentiate between azimuth and elevation rounding errors.

--- SirveApp/support/az_el_calculation.h
+++ SirveApp/support/az_el_calculation.h
@@ -7,14 +7,13 @@
 
 #include "support/earth.h"
 
+// referenced by AzElCalculation::calculate()
 const int FOCAL_PLANE_ARRAY_WIDTH = 640;
 const int FOCAL_PLANE_ARRAY_HEIGHT = 480;
 
-const int OSM_CENTROID_SHIFT = 1;
-
 namespace AzElCalculation
 {
-    std::vector<double> calculate(int x, int y, double sensor_lat, double sensor_long, std::vector<double> dcm, double ifov_x, double ifov_y, bool adjust_frame_ref, bool adjust_base, bool adjust_rounding_err);
+    std::vector<double> calculate(int x, int y, double sensor_lat, double sensor_long, std::vector<double> dcm, double ifov_x, double ifov_y, bool adjust_frame_ref, bool adjust_base, bool adjust_rounding_err_az, bool adjust_rounding_err_el);
 };
 
 #endif
