909db5fa1b38fb4a94904bfc16dd8dcffc9439cf
Kuhn <jordan.kuhn@maxar.com>
2024-04-10 08:28:25 -0400

    Clarify tests

--- SirveApp/support/az_el_calculation.h
+++ SirveApp/support/az_el_calculation.h
@@ -10,8 +10,6 @@
 const int FOCAL_PLANE_ARRAY_WIDTH = 640;
 const int FOCAL_PLANE_ARRAY_HEIGHT = 480;
 
-const int OSM_CENTROID_SHIFT = 1;
-
 namespace AzElCalculation
 {
     std::vector<double> calculate(int x, int y, double sensor_lat, double sensor_long, std::vector<double> dcm, double ifov_x, double ifov_y, bool adjust_frame_ref);
