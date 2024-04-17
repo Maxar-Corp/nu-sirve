6ea840605560990b588352488c466978a720f84c
DrAdamFriedman <130796725+DrAdamFriedman@users.noreply.github.com>
2024-04-17 17:26:18 -0400

    Refactor and hide shadow option
    Refactored background_subtraction to removed duplicate code and slightly modified the hide shadow algorithm/  Added rudimentary option to hide or show shadow but did not integrate into workspace

--- SirveApp/background_subtraction.h
+++ SirveApp/background_subtraction.h
@@ -11,13 +11,16 @@
 namespace AdaptiveNoiseSuppression
 {
 	std::vector<std::vector<double>> get_correction(int start_frame, int number_of_frames_input, video_details & original, QProgressDialog & progress);
-	std::vector<uint16_t> apply_correction(std::vector<uint16_t> frame, std::vector<double> correction);
 };
 
 namespace FixedNoiseSuppression
 {
 	std::vector<std::vector<double>> get_correction(int start_frame, int number_of_frames_input, video_details & original, QProgressDialog & progress);
-	std::vector<uint16_t> apply_correction(std::vector<uint16_t> frame, std::vector<double> correction);
+};
+
+namespace ApplyCorrection
+{
+	std::vector<uint16_t> apply_correction(std::vector<uint16_t> frame, std::vector<double> correction, QString & hide_shadow_choice);
 };
 
 #endif
