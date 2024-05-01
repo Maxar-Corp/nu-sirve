337ddfc8a4c9a95dc09be2b6d61c536613a902c6
Holland <david.holland@maxar.com>
2024-05-01 10:30:18 -0400

    Find config.json file, starting from current app location (QDir::currentPath())

--- SirveApp/config.h
+++ SirveApp/config.h
@@ -9,10 +9,18 @@
 #include <QIODevice>
 #include <QJsonDocument>
 #include <QJsonObject>
+#include <QString>
+#include <QDir>
+
+#include <iostream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 struct ConfigValues {
     double version;
     int max_used_bits;
+    QString workspace_folder;
 };
 
 namespace configreader
