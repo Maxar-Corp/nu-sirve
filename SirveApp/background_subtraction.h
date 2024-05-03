097a88c0e4a0568dee71a777bb42aea2e88bf8cb
DrAdamFriedman <130796725+DrAdamFriedman@users.noreply.github.com>
2024-05-02 20:55:44 -0400

    Cleaned up and refactored 240502
    Removed all double steps in noise suppression. Removed separate function for external file fixed noise suppression so both cases are handled the same. Fixes histogram issue with bad pixel removal. Rescaled noise suppression and removed previous ad hoc fix in sirveapp.

--- SirveApp/background_subtraction.h
+++ SirveApp/background_subtraction.h
@@ -16,22 +16,21 @@ namespace AdaptiveNoiseSuppression
 	std::vector<std::vector<uint16_t>> process_frames_conserve_memory(int start_frame, int number_of_frames_input, video_details & original, QString & hide_shadow_choice, QProgressDialog & progress);
 };
 
-namespace FixedNoiseSuppression
-{
-	std::vector<std::vector<uint16_t>> process_frames_fast(int start_frame, int number_of_frames, video_details & original, QProgressDialog & progress);
-	std::vector<std::vector<uint16_t>> process_frames(int start_frame, int number_of_frames_input, video_details & original, QProgressDialog & progress);
-};
+// namespace FixedNoiseSuppression
+// {
+// 	std::vector<std::vector<uint16_t>> process_frames(int start_frame, int number_of_frames_input, video_details & original, QProgressDialog & progress);
+// };
 
 namespace NoiseSuppressionGeneral
 {
 	void remove_shadow(arma::vec & frame_vector, arma::uvec index_negative, arma::uvec index_positive);
 };
 
-class FixedNoiseSuppressionExternal
+class FixedNoiseSuppression
 {
 public:
 
-	std::vector<std::vector<uint16_t>> process_frames(QString path_video_file, int start_frame, int end_frame, int number_of_frames, double version, video_details & original, QProgressDialog & progress);
+	std::vector<std::vector<uint16_t>> process_frames(AbpFileMetadata abp_file_metadata, QString path_video_file, int start_frame, int end_frame, double version, video_details & original, QProgressDialog & progress);
 
 private:
 	ABIR_Data abir_data;
