b03c573faf3ee87ff40b532852af5d02026dfa6f
DrAdamFriedman <130796725+DrAdamFriedman@users.noreply.github.com>
2024-05-04 14:56:54 -0400

    Fixed bugs and ready for merge
    I fixed significant bugs I found in my fixed noise suppression method and everything now appears to work as it should.

--- SirveApp/background_subtraction.cpp
+++ SirveApp/background_subtraction.cpp
@@ -2,21 +2,25 @@
 #include <vector>
 #include <random>
 
-std::vector<std::vector<uint16_t>> FixedNoiseSuppression::process_frames(AbpFileMetadata abp_file_metadata, QString path_video_file, int start_frame, int end_frame, double version, video_details & original, QProgressDialog & progress)
+std::vector<std::vector<uint16_t>> FixedNoiseSuppression::process_frames(QString image_path, QString path_video_file, int start_frame, int end_frame, double version, video_details & original, QProgressDialog & progress)
 {
 	// Initialize output
 	std::vector<std::vector<uint16_t>> frames_out;
 
 	int num_video_frames = original.frames_16bit.size();
 	int number_avg_frames;
-	
+	int  index_first_frame, index_last_frame;
+	index_first_frame = start_frame - 1;
+
 	ABIR_Data_Result abir_result;
-	int compare = QString::compare(path_video_file, abp_file_metadata.image_path, Qt::CaseInsensitive);
+	int compare = QString::compare(path_video_file, image_path, Qt::CaseInsensitive);
 		if (compare!=0){ 
-		abir_result = abir_data.Get_Frames(path_video_file.toLocal8Bit().constData(), start_frame, end_frame, version, false);
-		if (abir_result.had_error) {
-			return frames_out;
-		}
+			QByteArray array = image_path.toLocal8Bit();
+			char* buffer = array.data();
+			abir_result = abir_data.Get_Frames(buffer, start_frame, end_frame, version, false);
+			if (abir_result.had_error) {
+				return frames_out;
+			}
 		 number_avg_frames = abir_result.video_frames_16bit.size();
 		 progress.setWindowTitle("External Fixed Noise Suppression");
 	}
@@ -24,28 +28,34 @@ std::vector<std::vector<uint16_t>> FixedNoiseSuppression::process_frames(AbpFile
 		abir_result.video_frames_16bit = original.frames_16bit;
 		number_avg_frames = end_frame - start_frame + 1;
 	}
-
+	index_last_frame = index_first_frame + number_avg_frames - 1;
 	int num_pixels = abir_result.video_frames_16bit[0].size();
-
-    int index_first_frame = start_frame - 1;
-
-	int index_last_frame = std::min(index_first_frame + number_avg_frames - 1, num_video_frames - 1);
-	number_avg_frames = index_last_frame - index_first_frame + 1;
-
+ 	
 	// Create an Armadillo matrix for submatrix average
     arma::mat window_data(num_pixels, number_avg_frames);
 
-	int k = 0;
     // Fill the Armadillo matrix from the std::vector
-    for (int i = index_first_frame; i < index_last_frame; i++){
-        window_data.col(k) = arma::conv_to<arma::vec>::from(original.frames_16bit[i]);
-		k += 1;
+	if (compare!=0){ 
+		for (int i = 0; i < number_avg_frames; i++){
+			window_data.col(i) = arma::conv_to<arma::vec>::from(abir_result.video_frames_16bit[i]);
+		}
+	}
+	else{
+		int k = 0;
+		for (int i = index_first_frame; i < index_last_frame; i++){
+			window_data.col(k) = arma::conv_to<arma::vec>::from(abir_result.video_frames_16bit[i]);
+			k += 1;
+		}
     }
 
+	double tmpMax = window_data.max();
 	// Take the mean of each row
 	arma::vec mean_frame = arma::mean(window_data, 1);
+	mean_frame.save("mean_frame.bin",arma::arma_binary);
+	window_data.save("window_data.bin",arma::arma_binary);
 	
 	arma::vec frame_vector(num_pixels, 1);
+	arma::mat tmpMat(num_pixels,num_video_frames);
 	//Loop through frames to subtract mean
 	for (int i = 0; i < num_video_frames; i++){
 		if (progress.wasCanceled()){
@@ -55,10 +65,12 @@ std::vector<std::vector<uint16_t>> FixedNoiseSuppression::process_frames(AbpFile
 		frame_vector = arma::conv_to<arma::vec>::from(original.frames_16bit[i]);
 		frame_vector = frame_vector - mean_frame;
 		//Renormalize to 0-16383
-		frame_vector = frame_vector + abs(arma::min(frame_vector));
+		frame_vector = frame_vector - arma::min(frame_vector);
 		frame_vector = 16383 * frame_vector / frame_vector.max();
+		tmpMat.col(i) = frame_vector;
 		frames_out.push_back(arma::conv_to<std::vector<uint16_t>>::from(frame_vector));
     }
+	tmpMat.save("tmpMat.bin",arma::arma_binary);
 	return frames_out;
 }
 
@@ -96,14 +108,16 @@ std::vector<std::vector<uint16_t>> AdaptiveNoiseSuppression::process_frames_fast
 	frame_data = frame_data - moving_mean;
 	moving_mean.clear();
 	arma::vec frame_vector(num_pixels,1) ;
-	arma::rowvec min_values = arma::abs(arma::min(frame_data,0));
+	arma::rowvec min_values = arma::min(frame_data,0);
 	std::vector<std::vector<uint16_t>> frames_out;
+	arma::uvec index_negative;
+	arma::uvec index_positive;
 	if (hide_shadow_choice == "Hide Shadow"){
 		for (int i = 0; i < num_video_frames; i++){
 			progress.setValue(i + 2*num_video_frames);
-			arma::uvec index_negative = arma::find(frame_data.col(i) < 0);
-			arma::uvec index_positive = arma::find(frame_data.col(i) > 0);
-			frame_vector = frame_data.col(i) + min_values(i)*arma::ones(num_pixels,1);
+			index_negative = arma::find(frame_data.col(i) < 0);
+			index_positive = arma::find(frame_data.col(i) > 0);
+			frame_vector = frame_data.col(i) - min_values(i)*arma::ones(num_pixels,1);
 			NoiseSuppressionGeneral::remove_shadow(frame_vector, index_negative, index_positive);
 			frame_vector = 16383 * frame_vector / frame_vector.max();
 			frames_out.push_back(arma::conv_to<std::vector<uint16_t>>::from(frame_vector));
@@ -111,7 +125,7 @@ std::vector<std::vector<uint16_t>> AdaptiveNoiseSuppression::process_frames_fast
 		}
 	else
 	{
-		frame_data = frame_data + arma::repmat(min_values,num_pixels,1);
+		frame_data = frame_data - arma::repmat(min_values,num_pixels,1);
 		for (int i = 0; i < num_video_frames; i++){
 			progress.setValue(i + 2*num_video_frames);	
 			frame_vector = 16383 * frame_data.col(i) / frame_data.col(i).max();
@@ -158,11 +172,13 @@ std::vector<std::vector<uint16_t>> AdaptiveNoiseSuppression::process_frames_cons
 		window_data.shed_col(0);
 		moving_mean = arma::mean(window_data,1);
 		frame_vector = frame_vector - moving_mean;
-		min_value = abs(arma::min(frame_vector));
+		min_value = arma::min(frame_vector);
+		arma::uvec index_negative;
+		arma::uvec index_positive;
 		if (hide_shadow_choice == "Hide Shadow"){
-			arma::uvec index_negative = arma::find(frame_vector < 0);
-			arma::uvec index_positive = arma::find(frame_vector > 0);
-			frame_vector = frame_vector + min_value;
+			index_negative = arma::find(frame_vector < 0);
+			index_positive = arma::find(frame_vector > 0);
+			frame_vector = frame_vector - min_value;
 			NoiseSuppressionGeneral::remove_shadow(frame_vector, index_negative, index_positive);
 			frame_vector = 16383 * frame_vector / frame_vector.max();
 		}
