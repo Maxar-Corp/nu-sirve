0b247787c90065db31b1c14d01fc3b81c90ac3f9
DrAdamFriedman <130796725+DrAdamFriedman@users.noreply.github.com>
2024-06-05 18:20:23 -0400

    Style changes
    Minor style changes and refactoring

--- SirveApp/main.cpp
+++ SirveApp/main.cpp
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
 
 	QApplication a(argc, argv);
 
-    QFile file(":/Ubuntu.qss");
+    QFile file(":/stylesheets/Modified_Ubuntu.qss");
     if(file.open(QIODevice::ReadOnly | QIODevice::Text))
     {
         a.setStyleSheet(file.readAll());
