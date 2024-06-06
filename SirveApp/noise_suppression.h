163fc7c5679f003e88f686c6698f6f94627c67d9
DrAdamFriedman <130796725+DrAdamFriedman@users.noreply.github.com>
2024-06-05 21:54:04 -0400

    New shadow hiding method
    Implemented new shadow hiding algorithm

--- SirveApp/noise_suppression.h
+++ SirveApp/noise_suppression.h
@@ -9,26 +9,27 @@
 #include "process_file.h"
 #include "video_details.h"
 
-namespace AdaptiveNoiseSuppression
+class AdaptiveNoiseSuppression
 {
-	// std::vector<std::vector<uint16_t>> process_frames_fast(int start_frame, int number_of_frames_input, video_details & original, QString & hide_shadow_choice, QProgressDialog & progress);
+private:
+	arma::mat kernel;
+	void remove_shadow(int nRows, int nCols, arma::vec & frame_vector, arma::mat window_data, arma::vec moving_mean, int NThresh);	
+
+public:
+
 	std::vector<std::vector<uint16_t>> ProcessFramesConserveMemory(int start_frame, int number_of_frames_input, int NThresh, VideoDetails & original, QString & hide_shadow_choice, QProgressDialog & progress);
 };
 
-namespace NoiseSuppressionGeneral
-{
-	void remove_shadow(arma::vec & frame_vector, arma::mat window_data, arma::vec moving_mean, int NThresh);
-};
+
 
 class FixedNoiseSuppression
 {
-public:
-
-    std::vector<std::vector<uint16_t>> ProcessFrames(QString image_path, QString path_video_file, int start_frame, int end_frame, double version, VideoDetails & original, QProgressDialog & progress);
-
 private:
 	ABIRData abir_data;
 
+public:
+
+    std::vector<std::vector<uint16_t>> ProcessFrames(QString image_path, QString path_video_file, int start_frame, int end_frame, double version, VideoDetails & original, QProgressDialog & progress);
 };
 
 #endif
