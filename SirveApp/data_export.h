e677212020ef1aa55dbf55e266373e35323cd122
David Holland <david.holland@maxar.com>
2024-06-16 03:50:38 -0400

    Marshall manual track centroids.

--- SirveApp/data_export.h
+++ SirveApp/data_export.h
@@ -11,9 +11,13 @@
 
 namespace DataExport
 {
-    void WriteTrackDataToCsv(std::string save_path, std::vector<PlottingFrameData> frame_data,
-                                std::vector<PlottingTrackFrame> track_data, std::vector<ManualPlottingTrackFrame> manual_track_data,
-                         int min_frame = 0, int max_frame = 0, const std::vector<TrackFrame> osm_track_frame_data = std::vector<TrackFrame>());
+    void WriteTrackDataToCsv(   std::string save_path,
+                                std::vector<PlottingFrameData> frame_data,
+                                std::vector<PlottingTrackFrame> track_data,
+                                std::vector<ManualPlottingTrackFrame> manual_track_data,
+                                std::vector<TrackFrame> osm_track_frame_data = std::vector<TrackFrame>(),
+                                int min_frame = 0,
+                                int max_frame = 0);
 };
 
 #endif
