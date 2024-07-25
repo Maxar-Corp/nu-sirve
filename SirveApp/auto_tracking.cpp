dfb0e910070259c15d6f9eafca80908f2e1489ec
DrAdamFriedman <130796725+DrAdamFriedman@users.noreply.github.com>
2024-07-25 19:02:44 -0400

    Update auto_tracking.cpp
    cleaned up auto_tracking.  trying other filters. non local means

--- SirveApp/auto_tracking.cpp
+++ SirveApp/auto_tracking.cpp
@@ -29,58 +29,56 @@ arma::u32_mat AutoTracking::SingleTracker(u_int track_id, int frame0, int start_
     int ncols = original.x_pixels;
     u_int indx0 = start_frame - 1;
     arma::vec image_vector = arma::conv_to<arma::vec>::from(original.frames_16bit[indx0]);
-    image_vector = image_vector - arma::mean(image_vector);
-    image_vector.elem((arma::find(image_vector<0))).zeros();
+    // image_vector = image_vector - arma::mean(image_vector);
+    // image_vector.elem((arma::find(image_vector<0))).zeros();
+    image_vector = image_vector - image_vector.min();
+    image_vector = 255*image_vector/image_vector.max();
     int num_video_frames = original.frames_16bit.size();
+
     cv::Mat frame_matrix = cv::Mat(nrows,ncols,CV_64FC1,image_vector.memptr());
-    cv::Mat frame_matrix_blurred;
-    // cv:: Mat dst00;
-    // frame_matrix.convertTo(dst00, CV_32FC1);
-    // cv::bilateralFilter (dst00, frame_matrix_blurred, 9, 75, 75 );
-    cv::GaussianBlur(frame_matrix, frame_matrix_blurred, cv::Size(5,5), 0);
-    cv::Mat dst;
-    frame_matrix_blurred.convertTo(dst, CV_8UC1);
+    cv::Mat frame_matrix_8bit, frame_matrix_filtered_8bit, frame_matrix_filtered_8bit_color;
+    cv::Mat imCrop, imCrop_gray;
+    frame_matrix.convertTo(frame_matrix_8bit, CV_8UC1);
+    cv::fastNlMeansDenoising(frame_matrix_8bit, frame_matrix_filtered_8bit, 10,7,21);
+    cv::cvtColor(frame_matrix_filtered_8bit,frame_matrix_filtered_8bit_color,cv::COLOR_GRAY2RGB);
+
     Ptr<Tracker> tracker = TrackerMIL::create();
-    cv:: Mat dst1;
-    cv::cvtColor(dst,dst1,cv::COLOR_GRAY2RGB);
-    Rect ROI = cv::selectROI(dst1);
-    tracker->init(dst1,ROI);
-    std::vector<TrackFrame> track_frames_from_auto;
+    Rect ROI = cv::selectROI(frame_matrix_filtered_8bit_color);
+    tracker->init(frame_matrix_filtered_8bit_color,ROI);
+
     int num_frames = stop_frame - start_frame + 1;
     arma::u32_mat output(num_frames,4);
     u_int indx;
     for (u_int i = 0; i < num_frames; i++) {
         UpdateProgressBar(i);
         indx = (indx0 + i);
-        arma::vec image_vector_i = arma::conv_to<arma::vec>::from(original.frames_16bit[indx]);
-        image_vector_i = image_vector_i - arma::mean(image_vector_i);
-        image_vector_i.elem((arma::find(image_vector_i<0))).zeros();
-        cv::Mat frame_matrix_i = cv::Mat(nrows,ncols,CV_64FC1,image_vector_i.memptr());
-        cv::Mat frame_matrix_i_blurred;
-        cv::GaussianBlur(frame_matrix_i, frame_matrix_i_blurred, cv::Size(5,5), 0);
-        // cv::fastNlMeansDenoising(frame_matrix_i, frame_matrix_i_blurred, 10,7,21);
-        // cv:: Mat dst00i;
-        // frame_matrix_i.convertTo(dst00i, CV_32FC1);
-        // cv::bilateralFilter (dst00i, frame_matrix_i_blurred, 9, 75, 75);
-        cv::Mat dsti, dsti2;
-        frame_matrix_i_blurred.convertTo(dsti, CV_8UC1);
 
-        // cv::bilateralFilter (frame_matrix_i, frame_matrix_i_blurred, 11, 11*2, 11/2 );
-        cv:: Mat frame_matrix_ii;
-        cv::cvtColor(dsti,frame_matrix_ii,cv::COLOR_GRAY2RGB);
-        bool ok = tracker->update(frame_matrix_ii,ROI);
+        arma::vec image_vector_i = arma::conv_to<arma::vec>::from(original.frames_16bit[indx]);
+        // image_vector_i = image_vector_i - arma::mmean(image_vector_i);
+        // image_vector_i.elem((arma::find(image_vector_i<0))).zeros();
+        image_vector_i = image_vector_i - image_vector_i.min();
+        image_vector_i = 255*image_vector_i/image_vector_i.max();
+
+        cv::Mat frame_matrix_i = cv::Mat(nrows,ncols,CV_64FC1,image_vector_i.memptr());
+        cv::Mat frame_matrix_i_8bit, frame_matrix_i_filtered_8bit, frame_matrix_i_filtered_8bit_color, frame_matrix_i_filtered_color;
+        frame_matrix_i.convertTo(frame_matrix_i_8bit, CV_8UC1);
+        cv::fastNlMeansDenoising(frame_matrix_i_8bit, frame_matrix_i_filtered_8bit, 10,7,21);
+        frame_matrix_i_filtered_8bit.convertTo(frame_matrix_i_filtered_color,CV_32FC1);
+        cv::cvtColor(frame_matrix_i_filtered_8bit,frame_matrix_i_filtered_8bit_color,cv::COLOR_GRAY2RGB);
+        
+        bool ok = tracker->update(frame_matrix_i_filtered_8bit_color,ROI);
         if(ok){
-            rectangle(frame_matrix_ii, ROI, cv::Scalar( 0, 0, 255 ), 2);
-            imshow("tracker",frame_matrix_ii);
+            rectangle(frame_matrix_i_filtered_8bit_color, ROI, cv::Scalar( 0, 0, 255 ), 2);
+            imshow("tracker",frame_matrix_i_filtered_8bit_color);
         }
         else{
-            ROI = selectROI(frame_matrix_ii);
-            tracker->init(frame_matrix_ii,ROI);  
+            ROI = selectROI(frame_matrix_i_filtered_8bit_color);
+            tracker->init(frame_matrix_i_filtered_8bit_color,ROI);  
         }
         waitKey(1);
 
-        cv::Mat imCrop = dsti(ROI);
-        // cv::cvtColor(imCrop,dsti2,cv::COLOR_BGR2GRAY);
+        imCrop = frame_matrix_i_filtered_8bit(ROI);
+        // cv::cvtColor(imCrop,imCrop_gray,cv::COLOR_BGR2GRAY);
         cv::Mat thr;
         // cv::threshold(imCrop, thr, 100,255,cv::THRESH_BINARY);
         cv::threshold(imCrop, thr, 100,255,cv::THRESH_OTSU);
