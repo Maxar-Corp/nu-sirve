b1cd0e281cb77454403ff11eed7f40b058c28caf
David Holland <david.holland@maxar.com>
2024-08-30 11:07:19 -0400

    More lockdown code

--- SirveApp/SirveApp.h
+++ SirveApp/SirveApp.h
@@ -119,7 +119,7 @@ public:
 
 	QCheckBox * chk_auto_lift_gain, * chk_relative_histogram, * chk_plot_primary_data, * chk_plot_show_line, * chk_plot_full_data, * chk_hide_shadow, * chk_FNS_external_file;
 	QGroupBox * grpbox_auto_lift_gain, *grpbox_image_controls, *grpbox_colormap, *grpbox_overlay_controls, *grpbox_bad_pixels_correction, *grpbox_FNS_processing, *grpbox_ANS_processing, *grpbox_image_shift, *grpbox_status_area, *grpbox_image_processing;
-    QGroupBox *grpbox_load_frames_area, *grpbox_progressbar_area;
+    QGroupBox *grpbox_load_frames_area, *grpbox_progressbar_area, *plot_groupbox;
     QProgressBar * progress_bar_main;
 
 	QComboBox* cmb_deinterlace_options, * cmb_plot_yaxis, * cmb_plot_xaxis, *cmb_color_maps, * cmb_processing_states, * cmb_bad_pixels_type, * cmb_outlier_processing_type, *cmb_outlier_processing_sensitivity, *cmb_bad_pixel_color, *cmb_shadow_threshold;
