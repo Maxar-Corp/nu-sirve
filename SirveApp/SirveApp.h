6dd8e8d5ee7669a434360f1e1e037c405587b144
DrAdamFriedman <130796725+DrAdamFriedman@users.noreply.github.com>
2024-09-05 17:36:44 -0400

    GUI updates, refactor
    Added helper functions to enable and disable progress area and made sure they appear in each operation
    Cleaned up image processing and autotrack instantiation  labels
    Minor GUI layout fixes

--- SirveApp/SirveApp.h
+++ SirveApp/SirveApp.h
@@ -323,6 +323,8 @@ private:
     void ExportFrame();
     void ExportFrameRange();
     void ExportAllFrames();
+    void OpenProgressArea(QString message, int N);
+    void CloseProgressArea();
  
     void EnableEngineeringPlotOptions();
     void ExitTrackCreationMode();
