a11bdd9c50c26d90119fcc739382ab01b283792b
DrAdamFriedman <130796725+DrAdamFriedman@users.noreply.github.com>
2024-10-10 20:36:36 -0400

    Corrects crash when partial tracks are discarded in centered states
    Use case: Center video on a a track. Try to create a new auto track where it will probably fail (i.e. a dim point that vanishes or moves off frame). When the dialogue appears, hit cancel.

--- SirveApp/SirveApp.cpp
+++ SirveApp/SirveApp.cpp
@@ -4494,7 +4494,7 @@ void SirveApp::ExecuteAutoTracking()
         int threshold = 6 - cmb_autotrack_threshold->currentIndex();
         arma::u64_mat autotrack = AutoTracker.SingleTracker(track_id, clamp_low, clamp_high, threshold, prefilter, trackFeature, start_frame, start_frame_i, stop_frame_i, original.details, new_track_file_name);
         
-        if (video_display->container.processing_states[video_display->container.current_idx].offsets.size()>0){
+        if (!autotrack.empty() && video_display->container.processing_states[video_display->container.current_idx].offsets.size()>0){
             arma::vec framei = arma::regspace(start_frame_i,start_frame_i + autotrack.n_rows - 1);
             arma::mat offset_matrix2(framei.n_elem,3,arma::fill::zeros);
             std::vector<std::vector<int>> offsets = video_display->container.processing_states[video_display->container.current_idx].offsets;
