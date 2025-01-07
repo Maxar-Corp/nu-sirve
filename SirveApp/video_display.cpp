e5d9e036d16ce703c404f01e72419602bbfe4bc3
DrAdamFriedman <130796725+DrAdamFriedman@users.noreply.github.com>
2025-01-07 16:10:59 -0500

    Update video_display.cpp
    Changed variable passing to references in manual track creation to speed up and make usable.

--- SirveApp/video_display.cpp
+++ SirveApp/video_display.cpp
@@ -562,11 +562,11 @@ void VideoDisplay::SelectTrackCentroid(unsigned int x, unsigned int y)
     std::vector<uint16_t> frame_std_vector = {this->container.processing_states[this->container.current_idx].details.frames_16bit[this->counter].begin(),
            this->container.processing_states[this->container.current_idx].details.frames_16bit[this->counter].end()};  
     
-    processingState base_processing_state = this->container.processing_states[0];
+    processingState & base_processing_state = this->container.processing_states[0];
   
     for (auto ii = 0; ii < this->container.processing_states.size(); ii++)
     {
-        processingState test_state = this->container.processing_states[ii];
+        processingState & test_state = this->container.processing_states[ii];
         if (test_state.method == ProcessingMethod::replace_bad_pixels)
         {
             base_processing_state = test_state;
@@ -614,7 +614,7 @@ void VideoDisplay::SelectTrackCentroid(unsigned int x, unsigned int y)
     details.N_threshold_pixels = N_threshold_pixels;
     details.N_ROI_pixels = N_ROI_pixels;
     cv::Rect ROI2(minx + xCorrection,miny + yCorrection,ROI_width,ROI_height);
-    VideoDetails base_processing_state_details = base_processing_state.details;
+    VideoDetails & base_processing_state_details =  base_processing_state.details;
     details.irradiance =  IrradianceCountsCalc::ComputeIrradiance(this->counter, ROI_height/2, ROI_width/2, details.centroid_x, details.centroid_y, base_processing_state_details);
     details.ROI_x = minx + xCorrection;
     details.ROI_y = miny + yCorrection;
